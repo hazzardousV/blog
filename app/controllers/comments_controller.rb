37e0f6d6b9ff15d40c7d340c14c4b1846e7880e6
hazzardousV <quinweidner@gmail.com>
2016-01-24 04:35:16 -0600

    first launch

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+	before_filter :require_login, except: [:create]
 	def create
 		@comment = Comment.new(comment_params)
 		@comment.article_id = params[:article_id]
@@ -9,5 +10,10 @@ class CommentsController < ApplicationController
 	end
 	def comment_params
 		params.require(:comment).permit(:author_name, :body)
+	end
+	def destroy
+		@comment = Comment.find(params[:id])
+		@comment.destroy
+		redirect_to article_path
 	end
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,26 @@
 class TagsController < ApplicationController
+	before_filter :require_login, only: [:destroy]
 	def show
 		@tag = Tag.find(params[:id])
+	end
+	def index
+		@tags = Tag.all
+	end
+	def destroy
+		@tag = Tag.find(params[:id])
+		@tag.destroy
+
+		flash.notice = "'#{@tag.name}' will never be seen again!"
+		redirect_to tags_path
+	end
+	def edit
+		@tag = Tag.find(params[:id])
+	end
+	def update
+		@tag = Tag.find(params[:id])
+		@tag.update(name: params[:tag][:name])
+
+		flash.notice = "#{@tag.name}' has been changed"
+		redirect_to tags_path
 	end
 end
